763f2791cc5817eee829bfe2745e4642fe89744e
Adilson Mandlate <adilsonmandlate@gmail.com>
2023-07-22 01:20:33 +0200

    feat: design improvments

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,18 +1,19 @@
 import styles from "./Loader.module.css";
+import { motion } from "framer-motion";
 
 const LoaderComponents = () => {
   return (
     <>
       <div>
         {" "}
-        <span class={styles.skeleton_loader_background} />
+        <span className={styles.skeleton_loader_background} />
       </div>
       <br />
       <div>
-        <span class={styles.skeleton_loader_gradient} />
+        <span className={styles.skeleton_loader_gradient} />
       </div>
       <div>
-        <span class={styles.skeleton_loader} />
+        <span className={styles.skeleton_loader} />
       </div>
     </>
   );
@@ -20,12 +21,26 @@ const LoaderComponents = () => {
 
 const Loader = () => {
   return (
-    <div class={styles.skeleton_container}>
+    <div className={styles.skeleton_container}>
       <LoaderComponents />
       <br />
       <br />
       <LoaderComponents />
     </div>
+  );
+};
+
+export const RouteLoader = () => {
+  return (
+    <motion.div
+      initial={{ y: 50, opacity: 0 }}
+      animate={{ y: 0, opacity: 1, transition: { delay: 0 } }}
+      exit={{ y: 50, opacity: 0 }}
+      transition={{ ease: "easeInOut", duration: 0.3 }}
+      className={styles.route_loader}
+    >
+      Loading...
+    </motion.div>
   );
 };
 
